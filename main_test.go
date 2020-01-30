ddc549e90e777f38a4894ba43c6eea7bc31daee7
Behruz Valiev <behruzvaliev@gmail.com>
2020-01-30 21:40:07 +0500

    fix: fixes #2

--- main_test.go
+++ main_test.go
@@ -5,10 +5,22 @@ import (
 )
 
 func TestFuel(t *testing.T) {
-	rate := 22
-	fuel := 53
-	result := fuelConsumption(rate, fuel)
-	if result != 240 {
-		t.Error("Test error")
+	TestCases := []struct {
+		name   string
+		fuel   int
+		result int
+		rate   int
+		want   int
+	}{
+		{name: "Fuel more", fuel: 24, rate: 16, want: 150},
+		{name: "Fuel less", fuel: 14, rate: 16, want: 87},
+		{name: "Fuel equal", fuel: 15, rate: 15, want: 100},
+	}
+
+	for _, testCases := range TestCases {
+		got := fuelConsumption(testCases.rate, testCases.fuel)
+		if got != testCases.want {
+			t.Error("Test error")
+		}
 	}
 }
